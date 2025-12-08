67b85d88533fc820585872c191e6e0882e8a2296
talai-jpg <Talal1420alharthi@gmail.com>
2025-12-08 15:47:17 +0300

    added arabic messages

--- lib/core/comon/helper_function/biometric_helper.dart
+++ lib/core/comon/helper_function/biometric_helper.dart
@@ -2,45 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:la3bob/features/profiles/presentation/screens/profile_screen.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
 
 class BiometricHelper {
   static final LocalAuthentication _auth = LocalAuthentication();
 
-  static Future<bool> isBiometricSupported() async {
-    return await _auth.isDeviceSupported();
-  }
+  // static Future<bool> isBiometricSupported() async {
+  //   return await _auth.isDeviceSupported();
+  // }
 
-  static Future<List<BiometricType>> getAvailableBiometrics() async {
-    return await _auth.getAvailableBiometrics();
-  }
+  // static Future<List<BiometricType>> getAvailableBiometrics() async {
+  //   return await _auth.getAvailableBiometrics();
+  // }
 
   static Future<bool> authenticate() async {
     try {
       final bool didAuthenticate = await _auth.authenticate(
+        authMessages: <AuthMessages>[
+          // تخصيص رسائل الأندرويد
+          AndroidAuthMessages(
+            signInTitle: 'الرجاء التحقق من الهوية',
+            signInHint: 'استخدم البصمة أو الوجه',
+          ),
+        ],
         localizedReason: 'الرجاء التحقق من الهوية للمتابعة',
+        persistAcrossBackgrounding: true,
         biometricOnly: false,
       );
       return didAuthenticate;
     } catch (e) {
-      Fluttertoast.showToast(msg: "فشل التحقق  حاول مرة أخرى!");
+      Fluttertoast.showToast(
+        msg:
+            "فشل التحقق! الرجاء إعداد قفل للشاشة (رمز مرور، بصمة، أو وجه) للمتابعة.",
+      );
       return false;
     }
   }
 
   static Future<void> goToProfilePage(BuildContext context) async {
-    if (!await BiometricHelper.isBiometricSupported()) {
-      Fluttertoast.showToast(msg: "الجهاز لا يدعم المصادقة.");
-      return;
-    }
+    // if (!await BiometricHelper.isBiometricSupported()) {
+    //   Fluttertoast.showToast(msg: "الجهاز لا يدعم المصادقة.");
+    //   return;
+    // }
 
-    final availableBiometrics = await BiometricHelper.getAvailableBiometrics();
-    if (availableBiometrics.isEmpty) {
-      Fluttertoast.showToast(
-        msg:
-            "لم يتم العثور على مصادقة بصمة أو وجه الرجاء إعدادها في اعدادات الجهاز .",
-      );
-      return;
-    }
+    // final availableBiometrics = await BiometricHelper.getAvailableBiometrics();
+    // if (availableBiometrics.isEmpty) {
+    //   Fluttertoast.showToast(
+    //     msg:
+    //         "لم يتم العثور على مصادقة بصمة أو وجه الرجاء إعدادها في اعدادات الجهاز .",
+    //   );
+    //   return;
+    // }
 
     final bool didAuthenticate = await BiometricHelper.authenticate();
     if (didAuthenticate && context.mounted) {
