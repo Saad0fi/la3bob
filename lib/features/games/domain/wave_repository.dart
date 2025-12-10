29741a665cea968535a631e5423916cdb158ea26
Rager <0ooomar.2002@gmail.com>
2025-12-10 20:01:41 +0300

    fixed minor errors

--- lib/features/games/domain/wave_repository.dart
+++ lib/features/games/domain/wave_repository.dart
@@ -1,4 +1,4 @@
-import '../entities/wave_movement.dart';
+import 'package:la3bob/features/games/domain/entities/wave_movement.dart';
 import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 abstract class WaveRepository {
