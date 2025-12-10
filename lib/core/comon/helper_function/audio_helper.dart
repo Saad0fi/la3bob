da5911240bb1ece44909d970692cef39c58bb411
Saad0fi <saad.ofi@me.com>
2025-12-10 06:11:08 +0300

    fix problems

--- lib/core/comon/helper_function/audio_helper.dart
+++ lib/core/comon/helper_function/audio_helper.dart
@@ -4,42 +4,26 @@ class AudioHelper {
   static final AudioPlayer _player = AudioPlayer();
 
   static Future<void> playWordAudio(String word) async {
-    try {
-      await _player.stop();
-      await _player.play(AssetSource('audio/$word.m4a'));
-    } catch (e) {
-      print('Error playing audio for word $word: $e');
-    }
+    await _player.stop();
+    await _player.play(AssetSource('audio/$word.m4a'));
   }
 
   static Future<void> playSentenceAudio() async {
-    try {
-      await _player.stop();
-      await _player.play(AssetSource('audio/choose_letter.m4a'));
-    } catch (e) {
-      print('Error playing sentence audio: $e');
-    }
+    await _player.stop();
+    await _player.play(AssetSource('audio/choose_letter.m4a'));
   }
 
   static Future<void> playQuestionSequence(String word) async {
-    try {
-      await playWordAudio(word);
+    await playWordAudio(word);
 
-      await _player.onPlayerComplete.first;
-      await playSentenceAudio();
-      await _player.onPlayerComplete.first;
-      await playWordAudio(word);
-    } catch (e) {
-      print('Error playing question sequence: $e');
-    }
+    await _player.onPlayerComplete.first;
+    await playSentenceAudio();
+    await _player.onPlayerComplete.first;
+    await playWordAudio(word);
   }
 
   static Future<void> stopAudio() async {
-    try {
-      await _player.stop();
-    } catch (e) {
-      print('Error stopping audio: $e');
-    }
+    await _player.stop();
   }
 
   static void dispose() {
