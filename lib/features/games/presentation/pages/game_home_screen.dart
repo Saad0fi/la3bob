db606f0c21f3f68ed4f5282a79f7c06a324177b9
talai-jpg <Talal1420alharthi@gmail.com>
2025-12-17 22:45:35 +0300

    eal3eb eal3eb eaba6l eaba6l

--- lib/features/games/presentation/pages/game_home_screen.dart
+++ lib/features/games/presentation/pages/game_home_screen.dart
@@ -2,89 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:la3bob/core/comon/theme/app_color.dart';
+import 'package:la3bob/features/games/presentation/widgets/tab_bar.dart';
 import 'package:la3bob/features/profiles/presentation/bloc/porfile_bloc.dart';
-
-class GameCard extends StatelessWidget {
-  final String? route;
-  final String title;
-  final String imagePath;
-  final Color backgroundColor;
-
-  const GameCard({
-    super.key,
-    this.route,
-    required this.title,
-    required this.imagePath,
-    required this.backgroundColor,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => context.push(route!),
-      child:
-          Container(
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  borderRadius: BorderRadius.circular(20.dp),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      blurRadius: 10,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                          imagePath,
-                          height: 10.h,
-                          fit: BoxFit.contain,
-                          errorBuilder: (context, error, stackTrace) => Icon(
-                            Icons.videogame_asset,
-                            size: 40.dp,
-                            color: Colors.grey,
-                          ),
-                        )
-                        .animate(
-                          onPlay: (controller) =>
-                              controller.repeat(reverse: true),
-                        )
-                        .moveY(
-                          begin: -3,
-                          end: 3,
-                          duration: 1500.ms,
-                          curve: Curves.easeInOut,
-                        ),
-
-                    SizedBox(height: 1.h),
-
-                    Text(
-                      title,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16.dp,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.textSecondary,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-              .animate()
-              .fadeIn(duration: 400.ms)
-              .scale(
-                begin: const Offset(0.8, 0.8),
-                curve: Curves.easeOutBack,
-                duration: 500.ms,
-              ),
-    );
-  }
-}
 
 class GameHomeScreen extends StatelessWidget {
   const GameHomeScreen({super.key});
@@ -172,121 +92,13 @@ class GameHomeScreen extends StatelessWidget {
               ),
               const Expanded(
                 child: TabBarView(
-                  children: [_EducationalGamesTab(), _PhysicalGamesTab()],
+                  children: [EducationalGamesTab(), PhysicalGamesTab()],
                 ),
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class _EducationalGamesTab extends StatelessWidget {
-  const _EducationalGamesTab();
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> eduGames = [
-      {
-        'title': 'الألوان',
-        'route': '/tabs/games/colors',
-        'image': 'assets/images/color.png',
-        'color': const Color(0xFFF1F8E9),
-      },
-      {
-        'title': 'الأرقام',
-        'route': '/tabs/games/numbers',
-        'image': 'assets/images/numbers.png',
-        'color': const Color(0xFFF7F1FF),
-      },
-      {
-        'title': 'الحروف',
-        'route': '/tabs/games/letters',
-        'image': 'assets/images/letters.png',
-        'color': const Color(0xFFFFF3E0),
-      },
-      {
-        'title': 'التطابق',
-        'route': '/tabs/games/matching',
-        'image': 'assets/images/matching.png',
-        'color': const Color(0xFFE1F5FE),
-      },
-    ];
-
-    return GridView.builder(
-      padding: EdgeInsets.all(5.w),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 4.w,
-        mainAxisSpacing: 4.w,
-        childAspectRatio: 0.9,
-      ),
-      itemCount: eduGames.length,
-      itemBuilder: (context, index) {
-        final game = eduGames[index];
-        return GameCard(
-          title: game['title'],
-          route: game['route'],
-          imagePath: game['image'],
-          backgroundColor: game['color'],
-        );
-      },
-    );
-  }
-}
-
-class _PhysicalGamesTab extends StatelessWidget {
-  const _PhysicalGamesTab();
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> phyGames = [
-      {
-        'title': 'القرفصاء',
-        'route': '/games/squat',
-        'image': 'assets/images/gamer.png',
-        'color': const Color(0xFFF1F8E9),
-      },
-      {
-        'title': 'القفز',
-        'route': '/games/jump',
-        'image': 'assets/images/long-jump.png',
-        'color': const Color(0xFFF7F1FF),
-      },
-      {
-        'title': 'أوامر القائد',
-        'route': '/games/simon_says',
-        'image': 'assets/images/command.png',
-        'color': const Color(0xFFFFF3E0),
-      },
-      {
-        'title': 'حركة ستوب',
-        'route': '/games/freeze',
-        'image': 'assets/images/running.png',
-        'color': const Color(0xFFE1F5FE),
-      },
-    ];
-
-    return GridView.builder(
-      padding: EdgeInsets.all(5.w),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 4.w,
-        mainAxisSpacing: 4.w,
-        childAspectRatio: 0.9,
-      ),
-      itemCount: phyGames.length,
-      itemBuilder: (context, index) {
-        final game = phyGames[index];
-        return GameCard(
-          title: game['title'],
-          route: game['route'],
-          imagePath: game['image'],
-          backgroundColor: game['color'],
-        );
-      },
     );
   }
 }
